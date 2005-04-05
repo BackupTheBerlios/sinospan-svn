8ae69e8dbf0ec409449287ae64e273f2f19c6ad0
roothorick <roothorick@975dd889-8af3-0310-aade-9eed7ab1a589>
2005-04-05 01:28:24 +0000

    Why one-use functions? Clean things up in here.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -12,26 +12,6 @@ Still working on test environment.
 #include "out.h"
 #include "ui.h"
 
-bool mainInit();
-bool mainGo(int argc, char *argv[]);
-bool mainStop();
-void mainDie();
-
-int main(int argc, char *argv[])
-{
-	if(!mainInit() ) { return 64; }
-
-	mainGo(argc, argv);
-
-	UI::Loop();
-
-	mainStop();
-
-	mainDie();
-
-	return 0;
-}
-
 // Note on the various error names:
 // * FATALs happen when something happens that creates conditions under which
 //   it's unsafe to continue normal execution.
@@ -42,8 +22,9 @@ int main(int argc, char *argv[])
 // * DBGs aren't errors at all, they just tell me what's happening inside the
 //   program so I can track down bugs.
 
-bool mainInit()
+int main(int argc, char *argv[])
 {
+	// --- INITIALIZATION --- //
 	// I know how ugly this is, but it works, and it's efficient.
 	     if(!  ITC::Init() ) { printf("FATAL: main: Couldn't initialize ITC.\n"   ); }
 	else if(!Synth::Init() ) { printf("FATAL: main: Couldn't initialize synth.\n" ); }
@@ -51,42 +32,36 @@ bool mainInit()
 	// else if(!  Seq::Init() ) { printf("FATAL: main: Couldn't initialize seq IF.\n"); }
 	else if(!   UI::Init() ) { printf("FATAL: main: Couldn't initialize UI.\n"    ); }
 
-	else { return true; }
+	else
+	{
+		// --- ACTIVATION --- //
+		if(!  ITC::Go(          ) ) { printf("ERROR: main: Couldn't activate ITC.\n"   ); }
+		if(!Synth::Go(          ) ) { printf("ERROR: main: Couldn't activate synth.\n" ); }
+		if(!  Out::Go(          ) ) { printf("ERROR: main: Couldn't activate output.\n"); }
+		// if(!  Seq::Go(          ) ) { printf("ERROR: main: Couldn't activate seq IF.\n"); ret = false; }
+		if(!   UI::Go(argc, argv) ) { printf("ERROR: main: Couldn't activate UI.\n"    ); }
 
-	return false;
-}
 
-bool mainGo(int argc, char *argv[])
-{
-	bool ret = true;
+		// main loop...
+		UI::Loop();
 
-	if(!  ITC::Go(          ) ) { printf("ERROR: main: Couldn't activate ITC.\n"   ); ret = false; }
-	if(!Synth::Go(          ) ) { printf("ERROR: main: Couldn't activate synth.\n" ); ret = false; }
-	if(!  Out::Go(          ) ) { printf("ERROR: main: Couldn't activate output.\n"); ret = false; }
-	// if(!  Seq::Go(          ) ) { printf("ERROR: main: Couldn't activate seq IF.\n"); ret = false; }
-	if(!   UI::Go(argc, argv) ) { printf("ERROR: main: Couldn't activate UI.\n"    ); ret = false; }
+		// --- DEACTIVATION --- //
+		if(!   UI::Stop() ) { printf("ERROR: main: Couldn't deactivate UI.\n"    ); }
+		// if(!  Seq::Stop() ) { printf("ERROR: main: Couldn't deactivate seq IF.\n"); ret = false; }
+		if(!  Out::Stop() ) { printf("ERROR: main: Couldn't deactivate output.\n"); }
+		if(!Synth::Stop() ) { printf("ERROR: main: Clound't deactivate synth.\n" ); }
+		if(!  ITC::Stop() ) { printf("ERROR: main: Couldn't deactivate ITC.\n"   ); }
 
-	return ret;
-}
+		// -- DEINITIALIZATION --- //
+		UI::Die();
+		// Seq::Die();
+		Out::Die();
+		Synth::Die();
+		ITC::Die();
 
-bool mainStop()
-{
-	bool ret = true;
+		return 0;
+	}
 
-	if(!   UI::Stop() ) { printf("ERROR: main: Couldn't deactivate UI.\n"    ); ret = false; }
-	// if(!  Seq::Stop() ) { printf("ERROR: main: Couldn't deactivate seq IF.\n"); ret = false; }
-	if(!  Out::Stop() ) { printf("ERROR: main: Couldn't deactivate output.\n"); ret = false; }
-	if(!Synth::Stop() ) { printf("ERROR: main: Clound't deactivate synth.\n" ); ret = false; }
-	if(!  ITC::Stop() ) { printf("ERROR: main: Couldn't deactivate ITC.\n"   ); ret = false; }
-
-	return ret;
-}
-
-void mainDie()
-{
-	UI::Die();
-	// Seq::Die();
-	Out::Die();
-	Synth::Die();
-	ITC::Die();
+	// If initialization fails:
+	return 64;
 }
