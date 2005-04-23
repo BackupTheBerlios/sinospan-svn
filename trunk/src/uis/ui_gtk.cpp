66f257dee3bedbf6d028b9e844dfcc5ccef4ca40
roothorick <roothorick@975dd889-8af3-0310-aade-9eed7ab1a589>
2005-04-23 04:39:20 +0000

    Got cells working. Next big challenge will be some sort of module management infrastructure so the UI knows about different module types. (Also need to figure out some sort of interface for plugs / jacks as well.)

--- trunk/src/uis/ui_gtk.cpp
+++ trunk/src/uis/ui_gtk.cpp
@@ -2,16 +2,24 @@
    Licensed under the Open Software License version 2.1 */
 #include "ui_gtk.h"
 
+#include <map>
+
 #include <gtk/gtk.h>
 
+#include "cell.h"
 #include "itc.h"
 #include "track.h"
 #include "synth.h"
+
+typedef std::map<Track*, GtkWidget*> pTrackAssoc_t;
+pTrackAssoc_t pTrackAssoc;
 
 struct UI_GTK_CB
 {
 	static void AddTrack(GtkWidget*,gpointer);
 	static void RemoveTrack(GtkWidget*,gpointer);
+	static void AddCell(GtkWidget*,gpointer);
+	static void RemoveCell(GtkWidget*,gpointer);
 	static gboolean Delete(GtkWidget*,GdkEvent*,gpointer);
 	static void DestroyMain(GtkWidget*,gpointer);
 };
@@ -92,14 +100,19 @@ void UI_GTK_CB::AddTrack(GtkWidget *w, gpointer trackBook)
 			
 		gtk_box_pack_start(GTK_BOX(topBox), cellPort, TRUE, TRUE, 0);
 		gtk_widget_show(cellPort);
+		pTrackAssoc.insert(pTrackAssoc_t::value_type(trk, cellBox) );
 		
-		// TODO: Add Cell button
+		GtkWidget *addCellBut = gtk_button_new_with_label("Add Cell");
+		g_signal_connect(G_OBJECT(addCellBut), "clicked",
+			G_CALLBACK(UI_GTK_CB::AddCell), (gpointer) trk);
+		gtk_box_pack_start(GTK_BOX(topBox), addCellBut, FALSE, TRUE,
+									0);
+		gtk_widget_show(addCellBut);
 		
 		GtkWidget *remTrackBut = gtk_button_new_with_label(
 							"Remove Track");
 		g_signal_connect(G_OBJECT(remTrackBut), "clicked",
-					G_CALLBACK(UI_GTK_CB::RemoveTrack),
-							(gpointer) trk);
+			G_CALLBACK(UI_GTK_CB::RemoveTrack), (gpointer) trk);
 		gtk_box_pack_start(GTK_BOX(topBox), remTrackBut, FALSE, TRUE,
 									0);
 		gtk_widget_show(remTrackBut);
@@ -119,9 +132,65 @@ void UI_GTK_CB::RemoveTrack(GtkWidget *w, gpointer ptrk)
 		Synth::RemoveTrack( (Track*) trk);
 	});
 	// w is the remove track button. Parent for the toplevel container.
-	GtkWidget *twd = gtk_widget_get_parent(w);
-	gtk_widget_destroy(twd);
+	GtkWidget *topBox = gtk_widget_get_parent(w);
+	gtk_widget_destroy(topBox);
+	pTrackAssoc.erase(trk);
 	return;
+}
+
+void UI_GTK_CB::AddCell(GtkWidget *w, gpointer ptrk)
+{
+	Cell *cel = new Cell;
+	Track *trk = (Track*) ptrk;
+	// To save ourselves a lot of work (and lessen the load on ITC), use
+	// Cell::trk member to pass in the destined track.
+	cel->trk = trk;
+	ITC_DEFER(callback_0, ITC::OWNER_RENDER, pcel, (void*) cel,
+	{
+		Cell *cel = (Cell*) pcel;
+		cel->trk->AddCell(cel);
+	});
+	GtkWidget *cellBox = pTrackAssoc.find(trk)->second;
+	
+	GtkWidget *topBox = gtk_vbox_new(FALSE, 0);
+		
+		GtkWidget *mdlPort = gtk_scrolled_window_new(0x0, 0x0);
+		gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(mdlPort),
+				GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
+			
+			GtkWidget *mdlBox = gtk_vbox_new(FALSE, 0);
+			gtk_scrolled_window_add_with_viewport(
+					GTK_SCROLLED_WINDOW(mdlPort), mdlBox);
+			gtk_widget_show(mdlBox);
+			
+		gtk_box_pack_start(GTK_BOX(topBox), mdlPort, TRUE, TRUE, 0);
+		gtk_widget_show(mdlPort);
+		
+		// TODO: Add Module button
+		
+		GtkWidget *remCellBut = gtk_button_new_with_label(
+								"Remove Cell");
+		g_signal_connect(G_OBJECT(remCellBut), "clicked",
+			G_CALLBACK(UI_GTK_CB::RemoveCell), (gpointer) cel);
+		gtk_box_pack_start(GTK_BOX(topBox), remCellBut, FALSE, TRUE,
+									0);
+		gtk_widget_show(remCellBut);
+		
+	gtk_box_pack_start(GTK_BOX(cellBox), topBox, FALSE, TRUE, 0);
+	gtk_widget_show(topBox);
+}
+
+void UI_GTK_CB::RemoveCell(GtkWidget *w, gpointer pcel)
+{
+	Cell *cel = (Cell*) pcel;
+	ITC_DEFER(callback_0, ITC::OWNER_RENDER, pcel, (void*) cel,
+	{
+		Cell *cel = (Cell*) pcel;
+		cel->trk->RemoveCell(cel);
+	});
+	// We're at the "remove track" button; Parent is the topBox.
+	GtkWidget *topBox = gtk_widget_get_parent(w);
+	gtk_widget_destroy(topBox);
 }
 
 gboolean UI_GTK_CB::Delete(GtkWidget *w, GdkEvent *e, gpointer nul)
